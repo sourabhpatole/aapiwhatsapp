2a7c6be5d9f6c7ef16f2b9a3f1ae47e9e668a5e0
sourabhpatole <sourabhpatole@outlook.com>
2023-03-30 15:50:59 +0530

    main

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,6 +1,9 @@
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
+
 const VerifyToken = (req, res) => {
   try {
-    let accessToken = "QWERTYASDFGH";
+    let accessToken = process.env.ACCESS_TOKEN;
     let token = req.query["hub-verify_token"];
     let challenge = req.query["hub.challenge"];
     if (challenge != null && token != null && token == accessToken) {
@@ -14,7 +17,39 @@ const VerifyToken = (req, res) => {
 };
 
 const ReceiveMessages = (req, res) => {
-  res.send("Hi ReveiveToken!");
+  try {
+    let entry = req.query["entry"][0];
+    let changes = req.query["changes"][0];
+    let value = changes["value"];
+    let messageObject = value["message"];
+    let messages = messageObject[0];
+    let text = GetTextUser(messages);
+    res.send("EVENT_RECEIVED");
+  } catch (error) {
+    console.log(error);
+    res.send("EVENT_RECEIVED");
+  }
+};
+const GetTextUser = (messages) => {
+  let text = "";
+  let typeMessage = messages["type"];
+  if (typeMessage == "text") {
+    text = messages["text"]["body"];
+  } else if (typeMessage == "interactive") {
+    let interactiveObject = messages["interactive"];
+    let typeInteractive = interactiveObject["type"];
+    myConsole.log(interactiveObject);
+    if (typeInteractive == "button_replay") {
+      text = interactiveObject["button_replay"]["title"];
+    } else if (typeInteractive == "list_replay") {
+      text = interactiveObject["list_replay"]["title"];
+    } else {
+      myConsole.log("seen message");
+    }
+  } else {
+    myConsole.log("seen message");
+  }
+  return text
 };
 
 module.exports = { VerifyToken, ReceiveMessages };
