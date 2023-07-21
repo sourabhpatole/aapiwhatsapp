18c279f296d341153b65f3aa661dbc54d783865d
sourabhpatole <sourabhpatole@outlook.com>
2023-07-21 14:32:53 +0530

    master

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -24,7 +24,31 @@ const sendMessageWhatsapp = (data) => {
   req.write(data);
   req.end();
 };
+const sendMessageWhatsapp1 = (data) => {
+  const options = {
+    host: "graph.facebook.com",
+    path: "/v15.0/121276014242914/messages",
+    method: "POST",
+    body: data,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: process.env.WHATSAPP_TOKEN,
+    },
+  };
+  console.log(options.headers.Authorization);
+  const req = https.request(options, (res) => {
+    res.on("data", (d) => {
+      process.stdout.write(d);
+    });
+  });
+  req.on("error", (error) => {
+    console.error(error);
+  });
+  req.write(data);
+  req.end();
+};
 
 module.exports = {
   sendMessageWhatsapp,
+  sendMessageWhatsapp1,
 };
--- src/shared/processMessages.js
+++ src/shared/processMessages.js
@@ -1,6 +1,6 @@
 const {
   sendMessageWhatsapp,
-  sendMessageWhatsapp2,
+  sendMessageWhatsapp1,
 } = require("../services/whatsappService");
 const fs = require("fs");
 const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
@@ -17,38 +17,40 @@ const Process = (textUser, text, number) => {
   text = text.toLowerCase();
   console.log(text);
   let models = [];
+  let models1 = [];
   if (text.includes("hi")) {
     let model = MessageTemplateLunch(text, number);
 
-    // let model2 = MessageTemplateDinner(text, dinner);
+    let model1 = MessageTemplateDinner(text, dinner);
     models.push(model);
-  } else if (text == "lunch-veg") {
+    models1.push(model1);
+  } else if (text === "lunch-veg") {
     let model = MessageTemplateForVegL(
       `Thank you ${textUser} for choosing a vegetarian dish for _*Lunch*_. We will add it to our menu for you! `,
       number
     );
 
     models.push(model);
-  } else if (text == "lunch-non-veg") {
+  } else if (text === "lunch-non-veg") {
     let model = MessageTemplateForNonVegL(
       `Awesome, ${textUser} you have chosen non-vegetarian dish for _*Lunch*_. We will add it to our menu for you!`,
       number
     );
 
     models.push(model);
-  } else if (text == "dinner-veg") {
+  } else if (text === "dinner-veg") {
     let model = MessageTemplateForVegD(
       `Thank you ${textUser} for choosing a vegetarian dish for _*Dinner*_. We will add it to our menu for you! `,
       number
     );
 
-    models.push(model);
-  } else if (text == "dinner-non-veg") {
+    models1.push(model);
+  } else if (text === "dinner-non-veg") {
     let model = MessageTemplateForNonVegD(
       `Awesome, ${textUser} you have chosen non-vegetarian dish for _*Dinner*_. We will add it to our menu for you!`,
       number
     );
-    models.push(model);
+    models1.push(model);
   } else {
     let model = MessageText(
       `Thank you for your valuable time ${textUser}! If you have any feedback or suggestions on how we can improve our service, please don’t hesitate to let us know. We look forward to serving you again soon!`,
@@ -57,6 +59,7 @@ const Process = (textUser, text, number) => {
 
     // let model = MessageText("Thank You!", number);
     models.push(model);
+    models1.push(model);
 
     // } else {
     //   let model = MessageText("retry: " + text, number);
@@ -64,6 +67,7 @@ const Process = (textUser, text, number) => {
     //   models.push(model);
   }
   models.forEach((model) => sendMessageWhatsapp(model));
+  models1.forEach((model) => sendMessageWhatsapp1(model));
 };
 
 module.exports = { Process };
