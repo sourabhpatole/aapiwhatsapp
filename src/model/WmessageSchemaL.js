782d2c7cb7541f5108bece7d5dd43a5aa3d82273
sourabhpatole <sourabhpatole@outlook.com>
2023-08-09 13:27:26 +0530

    master

--- src/model/WmessageSchemaL.js
+++ src/model/WmessageSchemaL.js
@@ -3,10 +3,10 @@ const day = new Date(Date.now() + 3600 * 1000 * 24);
 const holiday = day.getDay();
 const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let newDay;
-if (dayNames[holiday] === "Wed") {
+if (dayNames[holiday] == "Wed") {
   newDay = new Date(Date.now() + 3600 * 1000 * 96);
 } else {
-  newDay = day;
+  newDay = new Date(Date.now() + 3600 * 1000 * 24);
 }
 
 const LunchWhatsappSchema = new mongoose.Schema(
