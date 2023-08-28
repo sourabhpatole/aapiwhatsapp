d13dcc96b80e1b7dab44eba292861030dcc3d270
sourabhpatole <sourabhpatole@outlook.com>
2023-08-28 17:54:54 +0530

    master

--- src/shared/processMessages.js
+++ src/shared/processMessages.js
@@ -52,7 +52,7 @@ const Process = async (textUser, text, number) => {
       models.push(model);
     }
   } else if (text == "lunch-non-veg") {
-    if (day >= 1 && day <= 5 && hours <= 21) {
+    if ((day >= 1 || day <= 5) && hours <= 21) {
       let model = MessageTemplateForFood(
         `Awesome, ${textUser} you have chosen non-vegetarian dish for _*Lunch*_. We will add it to our menu for you!`,
         number
@@ -68,7 +68,7 @@ const Process = async (textUser, text, number) => {
       models.push(model);
     }
   } else if (text == "dinner-veg") {
-    if (day >= 1 && day <= 5 && hours <= 14 && hours >= 8) {
+    if (!(day >= 1 || day <= 5) && (hours <= 14 || hours >= 8)) {
       let model = MessageTemplateForFood(
         `Thank you ${textUser} for choosing a vegetarian dish for _*Dinner*_. We will add it to our menu for you! `,
         number
@@ -84,7 +84,7 @@ const Process = async (textUser, text, number) => {
       models.push(model);
     }
   } else if (text == "dinner-non-veg") {
-    if (day >= 1 && day <= 5 && hours <= 14 && hours >= 8) {
+    if (!(day >= 1 || day <= 5) && (hours <= 14 || hours >= 8)) {
       let model = MessageTemplateForFood(
         `Awesome, ${textUser} you have chosen non-vegetarian dish for _*Dinner*_. We will add it to our menu for you!`,
         number
